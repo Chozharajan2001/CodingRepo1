be98c8c98b7e947aed0ae60ed28e26ce0e1020ca
Chozharajan2001 <93088046+Chozharajan2001@users.noreply.github.com>
2024-01-05 12:01:36 +0530

    Create RemoveWhiteSpace.java

---
+++ RemoveWhiteSpace.java
@@ -0,0 +1,20 @@
+//To remove all the white spaces in the string
+public class RemoveWhiteSpace{
+    public static void main(String[] args){
+        String word = " Hello World ";
+        removeSpace(word);
+    }
+    public static void removeSpace(String s){
+        if(s.length() == 0)
+            throw new IllegalArgumentException("String does not be null ");
+        StringBuilder word = new StringBuilder();
+        char[] charAr = s.toCharArray();
+        for(char ch : charAr){
+            if(ch != ' '){
+                word.append(ch);
+            }
+            
+        }
+        System.out.println(word);
+    }
+}
